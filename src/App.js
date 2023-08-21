d7e7111f7a9915af70503cfbdd5750bd4be514a8
Parshant <parshant4321@gmail.com>
2023-08-21 10:10:35 +0500

    App changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Babu you rock After changes
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
